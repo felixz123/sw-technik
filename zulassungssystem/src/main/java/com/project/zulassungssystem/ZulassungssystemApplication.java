a4401fb9c90fc178a6c4b9f9cda1352a604e95e6
Steffen Weisshaar <steffenweisshaar@yahoo.de>
2020-11-17 13:12:41 +0100

    Spring boot initial setup

---
+++ zulassungssystem/src/main/java/com/project/zulassungssystem/ZulassungssystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.zulassungssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZulassungssystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZulassungssystemApplication.class, args);
+	}
+
+}
---
+++ zulassungssystem/src/test/java/com/project/zulassungssystem/ZulassungssystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.zulassungssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZulassungssystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
