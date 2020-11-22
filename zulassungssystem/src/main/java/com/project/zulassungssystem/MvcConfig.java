34143e212a7f097731db81c1fc283148eb7c1107
Christian Schmitz <cschmi89@smail.uni-koeln.de>
2020-11-22 19:10:07 +0100

    basic navigation and responisive design

--- zulassungssystem/src/main/java/com/project/zulassungssystem/MvcConfig.java
+++ zulassungssystem/src/main/java/com/project/zulassungssystem/MvcConfig.java
@@ -8,7 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/home").setViewName("home");
-		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/about").setViewName("about");
+		registry.addViewController("/").setViewName("index");
 		registry.addViewController("/hello").setViewName("hello");
 		registry.addViewController("/zulassung").setViewName("zulassung");
 		registry.addViewController("/bewerber").setViewName("bewerber");
